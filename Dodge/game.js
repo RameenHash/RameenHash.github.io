10596f700ee2a2acbfdf588678c33fd16a30ca66
Rameen Hashemian <rameenhash@gmail.com>
2016-07-21 23:31:18 +0000

    Dodge plane

--- Dodge/game.js
+++ Dodge/game.js
@@ -2,25 +2,28 @@ var enemy;
 var isGameOver;
 var player;
 var playerImage;
-var enemy;
+var enemies = [];
 var enemyImage;
 var backgroundImage;
 
 function preload() {
-   playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png");
-   enemyImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png");
-   backgroundImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png")
+   playerImage = loadImage("plane.png");
+   enemyImage = loadImage("bird.jpg");
+   backgroundImage = loadImage("cloud.png")
 }
 
 function setup() {
-  createCanvas(256, 256);
-  
+   createCanvas(window.innerWidth, window.innerHeight);
+
    player = createSprite(width / 2, height - 25, 0, 0);
    player.addImage(playerImage);
-   enemy = createSprite(width / 2, height-(playerImage.height/2), 0, 0);
-   enemy.addImage(enemyImage);
-   enemy.rotationSpeed = 4.0;
-   
+   for (var i = 0; i <= 5; i++) {
+      var enemy = createSprite(width / 2, 0, 0, 0);
+      enemy.addImage(enemyImage);
+      enemies.push(enemy);
+   }
+   // enemy.rotationSpeed = 4.0;
+
    isGameOver = false;
 
 
@@ -31,7 +34,7 @@ function gameOver() {
    textAlign(CENTER);
    fill("white");
    text("Game Over!", width / 2, height / 2);
-   text("Click anywhere to try again", width/2, 3*height/4);
+   text("Click anywhere to try again", width / 2, 3 * height / 4);
 }
 
 
@@ -40,47 +43,47 @@ function draw() {
    if (isGameOver) {
       gameOver();
 
-      
-   } else {
 
-      if (enemy.overlap(player)) {
-         isGameOver = true;
-      }
-
-
+   }
+   else {
       background(backgroundImage);
 
-      if (keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage.width/2))) {
-         player.position.x +=2;
+      if (keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage.width / 2))) {
+         player.position.x += 5;
       }
 
-      if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width/2)) {
-         player.position.x -= 2;
+      if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width / 2)) {
+         player.position.x -= 5;
       }
 
-      enemy.position.y = enemy.position.y + 3;
+      for (var i = 0; i < enemies.length; i++) {
+         var enemy = enemies[i]
+         console.log(enemy)
+         if (enemy.overlap(player)) {
+            isGameOver = true;
+         }
+         enemy.position.y = enemy.position.y + 3;
 
-      if (enemy.position.y > height) {
-         enemy.position.y = 0;
-         enemy.position.x = random(5, width - 5);
+         if (enemy.position.y > height) {
+            enemy.position.y = 0;
+            enemy.position.x = random(5, width - 5);
+         }
       }
 
-      if (enemy.overlap(player)) {
-         isGameOver = true;
-
-      }
-
-  
       drawSprites();
    }
 }
 
- function mouseClicked() {
-      if (isGameOver) {
-     isGameOver = false;
-     player.position.x = width/2;
-     player.position.y = height-(playerImage.height/2);
-     enemy.position.x = width/2;
-     enemy.position.y = 0;
-    }
+function mouseClicked() {
+   if (isGameOver) {
+      for (var i = 0; i < enemies.length; i++) {
+         var enemy = enemies[i]
+
+         enemy.position.x = width / 2;
+         enemy.position.y = 0;
+      }
+      isGameOver = false;
+      player.position.x = width / 2;
+      player.position.y = height - (playerImage.height / 2);
    }
+}
